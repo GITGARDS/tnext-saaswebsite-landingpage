eae3b02c1b27a13891d5acc2a22ccd75476fac07
GITGARDS <adrianorogeriodesouza4353@gmail.com>
2025-01-21 16:15:44 -0300

    feat: hero

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import { DM_Sans } from "next/font/google";
 import "./globals.css";
 
 const dmSans = DM_Sans({
-  variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
@@ -19,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-Br">
       <body className={clsx(dmSans.className, "antialiased")}>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,14 @@
 import { Banner } from "@/components/Banner";
-import { CallToAction } from "@/components/CallToAction";
-import { FAQs } from "@/components/FAQs";
-import { Features } from "@/components/Features";
-import { Footer } from "@/components/Footer";
 import { Hero } from "@/components/Hero";
-import { LogoTicker } from "@/components/LogoTicker";
 import { Navbar } from "@/components/Navbar";
-import { ProductShowcase } from "@/components/ProductShowcase";
 
 export default function Home() {
   return (
     <>
       <Banner />
-      <hr />
       <Navbar />
-      <hr />
       <Hero />
-      <hr />
+      {/* <hr />
       <LogoTicker />
       <hr />
       <Features />
@@ -27,7 +19,7 @@ export default function Home() {
       <hr />
       <CallToAction />
       <hr />
-      <Footer />
+      <Footer /> */}
     </>
   );
 }

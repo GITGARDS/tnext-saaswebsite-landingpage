fe155ca9a221fd426a6f342585f7e3b3e2a50d87
GITGARDS <adrianorogeriodesouza4353@gmail.com>
2025-01-21 15:14:00 -0300

    feat: barner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,14 @@
 export const Banner = () => {
   return (
-    <>
-      <div>Banner</div>
-    </>
+    <div className="py-3 text-center bg-gradient-to-r from-rose-200 via-yellow-300  to-rose-200">
+      <div className="container">
+        <p className="font-medium">
+          <span className="hidden sm:inline">Introducing a completely redesigned interface - </span>
+          <a href="#" className="underline underline-offset-4">
+            Explore the demo
+          </a>
+        </p>
+      </div>
+    </div>
   );
 };

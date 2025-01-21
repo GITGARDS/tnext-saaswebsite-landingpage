29da2b0f509e40f4130cbcd12134cccb3d05b2a1
GITGARDS <adrianorogeriodesouza4353@gmail.com>
2025-01-21 15:14:26 -0300

    feat: navbar

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,20 @@
 export const Hero = () => {
   return (
-    <>
-      <div>Hero</div>
-    </>
+    <div className="bg-black text-white bg-gradient-to-b from-[#000] via-[#200D42_34%] to-[#4f21a1_82%] py-[72px]">
+      <div className="container">
+        <div className="flex items-center justify-center">
+          <a href="#" className="border py-1 px-2 rounded-lg border-white/30">
+            <span>Version 2.0 is here</span>
+            <span>Read More</span>
+          </a>
+        </div>
+        <h1>One Task at a Time</h1>
+        <p>
+          Celebrate the joy of accomplishment with an app desegined to track
+          progress, motivate your efforts, and celebrate your successes.
+        </p>
+        <button>Get for free</button>
+      </div>
+    </div>
   );
 };

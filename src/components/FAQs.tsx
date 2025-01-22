ec98e2336758bf9b7104cd590ac70dec92510165
GITGARDS <adrianorogeriodesouza4353@gmail.com>
2025-01-22 11:17:05 -0300

    feat: FAQs

--- src/components/FAQs.tsx
+++ src/components/FAQs.tsx
@@ -1,5 +1,5 @@
 "use client";
-import clsx from "clsx";
+import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 import MinusIcon from "../assets/icons/minus.svg";
 import PlusIcon from "../assets/icons/plus.svg";
@@ -67,14 +67,29 @@ const AccordionItem = ({
         <span className="flex-1 ltext-lg font-bold">{question}</span>
         {isOpen ? <MinusIcon /> : <PlusIcon />}
       </div>
-      <div
-        className={clsx("mt-4 cursor-default", {
-          hidden: !isOpen,
-          "": isOpen === true,
-        })}
-      >
-        {answer}
-      </div>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{
+              opacity: 0,
+              height: 0,
+              marginTop: 0,
+            }}
+            animate={{
+              opacity: 1,
+              height: "auto",
+              marginTop: "16px",
+            }}
+            exit={{
+              opacity: 0,
+              height: 0,
+              marginTop: 0,
+            }}
+          >
+            {answer}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
